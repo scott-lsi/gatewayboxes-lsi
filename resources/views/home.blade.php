53700922b7559d645a2879f4bc8a7336a9bdf297
Robert Hemingway <rhemingway@lsi.local>
2019-08-22 15:39:01 +0100

    Small changes to modal window sizes and email changes
    Small changes to modal size of window so easier to read as well as the emails being changed.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,10 @@
             <!-- <h3 class="text-muted">You are logged in.</h3> -->
         </div>
         <div class="col-sm-8 text-right">
-            <p class="lead">Oliver: <span class="text-primary">01274 854963</span><br>Dan: <span class="text-primary">01274 854977</span><br>Email: <span class="text-primary">sales@thinkinsidethebox.online</span></p>
+            <p style="font-size: 1.6rem;">Oliver: <span class="text-primary">01274 854963</span>
+            <br><span class="text-primary">Oliver@thinkinsidethebox.online</span><br>
+            Dan: <span class="text-primary">01274 854977</span><br>
+            <span class="text-primary">Dan@thinkinsidethebox.online</span></p>
         </div>
     </div>
 
@@ -18,7 +21,7 @@
 
     <!-- WELCOME SECTION -->
     <div class="row">
-        <div class="col-sm-2">
+        <div class="col-sm-2 text-center">
             <img src="{{ asset('images/homepage/welcome.jpg') }}" style="width: 150px;" alt="Welcome">
         </div>
         <div class="col-sm-10 text-center">
@@ -59,18 +62,20 @@
 
             <!-- Modal content-->
             <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">First Impressions</h4>
-            </div>
-            <div class="modal-body">
-                <p>Bespoke boxes are a fantastic way to add value to yours or your clients merchandise/giveaways. 
-                Gift boxes are used currently for product launches, onboarding gifts, thankyou and commemorative gifts. 
-                Help your customers stay ahead of their competition and create a positive first impression.</p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            </div>
+                <div class="container">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">First Impressions</h4>
+                    </div>
+                    <div class="modal-body">
+                        <p>Bespoke boxes are a fantastic way to add value to yours or your clients merchandise/giveaways. 
+                        Gift boxes are used currently for product launches, onboarding gifts, thankyou and commemorative gifts. 
+                        Help your customers stay ahead of their competition and create a positive first impression.</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -80,18 +85,20 @@
 
             <!-- Modal content-->
             <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Help Line</h4>
-            </div>
-            <div class="modal-body">
-                <p>Once you have gained your clients interest in the product and discussed what products may feature in the box,
-                please call <strong class="text-primary">Dan on 01274 854977</strong> or <strong class="text-primary">Oliver on 01274 854963</strong> for help and advice. They will guide you through the process
-                of housing products in the box and provide layout designs free of cost.</p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            </div>
+                <div class="container">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Help Line</h4>
+                    </div>
+                    <div class="modal-body">
+                        <p>Once you have gained your clients interest in the product and discussed what products may feature in the box,
+                        please call <strong class="text-primary">Dan on 01274 854977</strong> or <strong class="text-primary">Oliver on 01274 854963</strong> for help and advice. They will guide you through the process
+                        of housing products in the box and provide layout designs free of cost.</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -101,26 +108,28 @@
 
             <!-- Modal content-->
             <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Terms of Use</h4>
-            </div>
-            <div class="modal-body">
-                <p>
-                    As a new user your first 10 box designs are free of charge. Following that we will invoice you monthly for any box
-                    designs you order via the portal (Payment terms are 30 days nett of month end). Late payment of invoices will result in your
-                    access to the portal being suspended.<br>
-                    Boxes are limited to 1 box 1 design - should you require more than 1 box for your chosen design, you just speak to 
-                    Dan or Oliver who can help.
-                </p>
-                <p>
-                    Once your client is happy with the proposed layout, should you wish to see a finished sample with foam inlay this laser
-                    sample is charged at £25, any subsequent changes to the layout and further samples will be charged at £15 each.
-                </p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            </div>
+                <div class="container">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Terms of Use</h4>
+                    </div>
+                    <div class="modal-body">
+                        <p>
+                            As a new user your first 10 box designs are free of charge. Following that we will invoice you monthly for any box
+                            designs you order via the portal (Payment terms are 30 days nett of month end). Late payment of invoices will result in your
+                            access to the portal being suspended.<br>
+                            Boxes are limited to 1 box 1 design - should you require more than 1 box for your chosen design, you just speak to 
+                            Dan or Oliver who can help.
+                        </p>
+                        <p>
+                            Once your client is happy with the proposed layout, should you wish to see a finished sample with foam inlay this laser
+                            sample is charged at £25, any subsequent changes to the layout and further samples will be charged at £15 each.
+                        </p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
